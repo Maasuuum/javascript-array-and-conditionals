40a402f6dc287b74bfd36e54222a5d49b1711bd0
Maasuuum <fardousalmasum09@gmail.com>
2024-02-05 16:44:39 +0600

    Push and Pop of Array

---
+++ push-pop.js
@@ -0,0 +1,18 @@
+var numbers = [78, 45, 98, 45];
+
+// something new value adding process in arrays. Use push() to add element to an array as the last element array
+// numbers.push(63);
+console.log(numbers);
+
+var friends = ['balam', 'kalam', 'salam'];
+friends.push('gelam');
+friends.push('pailam');
+console.log(friends);
+
+// use pop to loss last element
+numbers.pop();
+console.log(numbers);
+
+// way to know the pop element
+var element = numbers.pop();
+console.log(element);
