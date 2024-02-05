c0a41321a573c598b47e1b701b2332dc5336950b
Maasuuum <fardousalmasum09@gmail.com>
2024-02-05 15:35:08 +0600

    details about index

---
+++ index.js
@@ -0,0 +1,19 @@
+var numbers= [45, 68, 78, 56, 89, 98];
+
+// get element by index
+console.log(numbers);
+
+// to know index value we have to use [] and index number
+console.log(numbers[1]);
+// another way to know the element 
+var element = numbers[2];
+console.log(element);
+
+// to change/set elements new value we use arrayname[index no] = new value;
+numbers[3] = 77;
+console.log(numbers);
+
+// find index of an element
+// var positionIndex = numbers.indexOf(89);
+// console.log(positionIndex);
+console.log(numbers.indexOf(89));
