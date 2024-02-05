deb6ba2eccf3406023f7c5b7e53cb30de41aeced
Maasuuum <fardousalmasum09@gmail.com>
2024-02-05 12:27:11 +0600

    Details about Variable and Operations

---
+++ variable.js
@@ -0,0 +1,33 @@
+// Number type variable
+var sunglassPrice = 120;
+
+// String type variable
+var specialName = "Malm Bella";
+
+// Boolean type variable
+var isSingle = false;
+
+// Types of Number: Integer-120,1,10; Float/Decimal: 0.1,3.65 etc.
+
+// Variable naming rules:
+/**
+ * 1. Must be in a single word
+ * 2. If two words then no gap/spaces 
+ * 3. No quote
+ * 4. No keyword/Reserved word
+ * 5. Can not start with a number
+ * 6. Should use Snake case, Camelcase, Pascale Case: user_name, userName, UserName
+ */
+
+// Math Operations: +, -, *, /, %
+// Shorthand:
+/**
+ * 1.Addition in two variables(+=)
+ * 2.Substraction in two variables(-=)
+ * 3.Add 1 to the existing value(++)
+ * 4.Deduct 1 to the existing value(--)
+ */
+
+// Convert: parseInt, parseFloat
+
+// Indicate to how many numbers will consider after decimal we use (toFixed(Digit))
