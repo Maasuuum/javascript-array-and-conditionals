372d700d5a097faeadc5a1d9fcd0fe0e5f9101c7
Maasuuum <fardousalmasum09@gmail.com>
2024-02-05 13:10:15 +0600

    Declare Array,Array's Length and Index

---
+++ array.js
@@ -0,0 +1,16 @@
+// Array Declaration:
+/**
+ * 1.Must use []Third Bracket to the values
+ * 2.Must use ,Comma between two values
+ * 3.Space between two values(Not mandatory)
+ */
+var friendAge = [11, 21, 45, 17, 14, 105, 6];
+var picnicFee = [5000, 2000, 4000, 150];
+var nayikas = ['Mahi', 'Opu', 'Sabnoor', 'Sabana'];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+console.log(vowels);
+// To show how many items in a arrays use .length
+console.log(vowels.length);
+
+// Index in arrays: first element/item of arrays always will be count as 0 then 1, 2, 3, .... like this.Ex: var vowels = ['a', 'e', 'i', 'o', 'u']; Here 'a' elements index is 0 and 'u' element's index is 4.
