5183b64e796de5c791d16e888e003a4427c2d988
Maasuuum <fardousalmasum09@gmail.com>
2024-02-07 15:15:33 +0600

    comparison

---
+++ comparison.js
@@ -0,0 +1,16 @@
+// console.log(5 < 6);
+// console.log(5 == 6);
+
+var num1 = 56;
+var num2 = 78;
+
+console.log(num1 < num2);
+console.log(num1 > num2);
+console.log(num1 == num2);
+console.log(num1 != num2);
+console.log(num1 <= num2);
+console.log(num1 >= num2);
+
+// multiple conditions
+// money > money2 && result1 > result2 && height1 > height2
+// money > money2 || result1 > result2 || height1 > height2
