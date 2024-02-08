d76f49e3c3fc956f2e892b6ac80202678434c1ad
Maasuuum <fardousalmasum09@gmail.com>
2024-02-08 23:47:13 +0600

    and or conditions

---
+++ multiple-conditions.js
@@ -0,0 +1,24 @@
+var isGraduated = true;
+var salary = 75000;
+var cars = 1;
+
+// if(isGraduated == true){
+//     console.log('Eso biye kore feli')
+// }
+// else{
+//     console.log('tor kopale biya nai')
+// }
+
+if(isGraduated == true && salary > 50000 && cars >= 1){
+    console.log('Eso biye kore feli')
+}
+else{
+    console.log('tor kopale biya nai')
+}
+
+if(isGraduated == false || salary > 750000){
+    console.log('Eso biye kore feli')
+}
+else{
+    console.log('time nai')
+}
